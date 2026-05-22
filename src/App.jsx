aeb2571f4e950f56662d8518224bd5e1c7575ae2
GiacomoVisin <killerboss1150@gmail.com>
2026-05-22 19:18:39 +0200

    milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Products from "./components/Products"
 import ProductPage from "./components/ProductPage"
 import Page404 from "./components/Page404"
 import { useEffect, useState } from "react"
+import { BudgetProvider } from "./assets/contexts/BudgetProvider"
 
 function App() {
 
@@ -21,17 +22,19 @@ function App() {
 
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" Component={Layout}>
-            <Route index Component={Home} />
-            <Route path="chi-siamo" Component={About} />
-            <Route path="prodotti" element={<Products products={products}/>} />
-            <Route path="prodotti/:id" Component={ProductPage} />
-            <Route path="*" Component={Page404} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <BudgetProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" Component={Layout}>
+              <Route index Component={Home} />
+              <Route path="chi-siamo" Component={About} />
+              <Route path="prodotti" element={<Products products={products} />} />
+              <Route path="prodotti/:id" Component={ProductPage} />
+              <Route path="*" Component={Page404} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </BudgetProvider>
     </>
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,15 @@
+import { createContext, useContext } from "react";
 import { NavLink } from "react-router-dom"
+import { BudgetContext } from "../assets/contexts/BudgetProvider"
+
 
 
 export default function NavBar() {
 
+    const { budgetMode, setBudgetMode } = useContext(BudgetContext)
+
     return (
+
         <nav style={{ display: "flex", gap: "15px", padding: "10px", background: "#eee" }}>
             <NavLink to="/" className="navlink"> Home </NavLink>
 
@@ -11,5 +17,11 @@ export default function NavBar() {
 
             <NavLink to="/prodotti" className="navlink"> Prodotti </NavLink>
         </nav>
+    
+    
+
     );
+
+
+
 }
